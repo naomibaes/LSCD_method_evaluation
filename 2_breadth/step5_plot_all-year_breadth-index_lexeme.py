5231d5dbbc1421214d97df2f25204a2068a3e259
Naomi Baes <naomi_baes@hotmail.com>
2025-01-22 12:34:21 +1100

    Update all-year plot labels

--- 2_breadth/step5_plot_all-year_breadth-index_lexeme.py
+++ 2_breadth/step5_plot_all-year_breadth-index_lexeme.py
@@ -6,38 +6,40 @@ import numpy as np
 # Load the data from the CSV file
 try:
     df = pd.read_csv("output/baseline_final_combined.all-year.cds_lexeme.csv")
+    # Ensure the term names are formatted properly before filtering
+    df['term'] = df['term'].str.replace('_', ' ').str.title()
     print("Successfully read data from CSV file.")
 except FileNotFoundError:
     print("Error: CSV file not found. Please check the file path.")
 
 # Define colors, line styles, and marker styles
 colors = {
-    'abuse': '#8B0000',
-    'anxiety': '#FF6347',
-    'depression': '#4B0082',
-    'mental_health': '#008080',
-    'mental_illness': '#800080',
-    'trauma': '#DC143C',
+    'Abuse': '#8B0000',
+    'Anxiety': '#FF6347',
+    'Depression': '#4B0082',
+    'Mental Health': '#008080',
+    'Mental Illness': '#800080',
+    'Trauma': '#DC143C',
 }
 line_styles = {
-    'abuse': '-',
-    'anxiety': '--',
-    'depression': '-.',
-    'mental_health': ':',
-    'mental_illness': (0, (3, 10, 1, 10)),
-    'trauma': (0, (5, 1))
+    'Abuse': '-',
+    'Anxiety': '--',
+    'Depression': '-.',
+    'Mental Health': ':',
+    'Mental Illness': (0, (3, 10, 1, 10)),
+    'Trauma': (0, (5, 1))
 }
 marker_style = {
-    'abuse': 'o',
-    'anxiety': 's',
-    'depression': 'D',
-    'mental_health': '^',
-    'mental_illness': 'v',
-    'trauma': 'P'
+    'Abuse': 'o',
+    'Anxiety': 's',
+    'Depression': 'D',
+    'Mental Health': '^',
+    'Mental Illness': 'v',
+    'Trauma': 'P'
 }
 
 # List of target terms
-sorted_target_terms = sorted(['abuse', 'anxiety', 'depression', 'mental_health', 'mental_illness', 'trauma'])
+sorted_target_terms = sorted(['Abuse', 'Anxiety', 'Depression', 'Mental Health', 'Mental Illness', 'Trauma'])
 
 # Create the plot
 fig, ax = plt.subplots(figsize=(14, 10.1))
@@ -59,11 +61,13 @@ for term in sorted_target_terms:
 ax.axvspan(10, 90, color='grey', alpha=0.2)
 ax.axvspan(90, 105, color='darkgrey', alpha=0.5)
 
+ax.text(0.03, 0.95, 'Diverse Contexts Injection', transform=ax.transAxes, fontsize=28, verticalalignment='top')
+
 # Set y-axis limits and ticks
 y_min = 0
-y_max = 0.35  # Fixed at 0.76
+y_max = 0.36
 ax.set_ylim(y_min, y_max)
-y_ticks = np.arange(y_min, y_max + 0.05, 0.05)  # Adjusted increment to 0.02
+y_ticks = np.arange(y_min, y_max + 0.05, 0.05)
 ax.set_yticks(y_ticks)
 
 # Axis labels and ticks
@@ -76,7 +80,7 @@ ax.yaxis.set_major_formatter(FuncFormatter(format_y_ticks))
 ax.tick_params(axis='both', labelsize=28)
 
 # Legend
-handles = [plt.Line2D([0], [0], color=colors[term], linestyle=line_styles[term], marker=marker_style[term], markersize=14, linewidth=3, label=f'{term.capitalize()}') for term in sorted_target_terms]
+handles = [plt.Line2D([0], [0], color=colors[term], linestyle=line_styles[term], marker=marker_style[term], markersize=14, linewidth=3, label=term) for term in sorted_target_terms]
 legend = ax.legend(handles=handles, loc="upper center", bbox_to_anchor=(0.5, -0.18), fontsize=30, ncol=3, frameon=False, title="Target Terms")
 plt.setp(legend.get_title(), fontsize=30)
 
